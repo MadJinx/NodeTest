b5d57cf60488dfd81481cded83ae85ad479c6df9
Ryan Beverly <ryan@aspencloud.com>
2013-10-07 14:14:39 -0600

    refactored hello_word.js into a module

--- hello_world.js
+++ hello_world.js
@@ -1,7 +1,12 @@
 var http = require('http');
 
-http.createServer( function(request, response) {
-	response.writeHead(200, {"Content-Type":"text/plain"});
-	response.write("Hello World!");
-	response.end();
-}).listen(8000);
+function start() {
+	function onRequest(request, response) {
+		response.writeHead(200, {"Content-Type":"text/plain"});
+		response.write("Hello World!");
+		response.end();
+	}
+	http.createServer(onRequest).listen(8000);
+}
+
+exports.start = start;
